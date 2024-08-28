ddff7fadf29da0aedeee30a53189e62dfbf44dab
shumodi <shumodi@deloitte.com>
2024-08-28 21:59:38 +0530

    created schmea model to tell how data should look like

--- models/todoModel.js
+++ models/todoModel.js
@@ -1,1 +1,14 @@
-const mongoose
+const mongoose = require('mongoose');
+
+const Schema  = mongoose.Schema;
+
+var toDoSchema = new Schema({
+    username: String,
+    toDo: String,
+    isDone: Boolean,
+    hasAttachment: Boolean
+});
+
+var ToDo = mongoose.model('ToDo',toDoSchema );
+
+module.exports = ToDo;
